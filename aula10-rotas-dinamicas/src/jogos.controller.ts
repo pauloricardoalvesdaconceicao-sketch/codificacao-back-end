525fa948376822d8073adba406f6a49a13fbfd1a
paulo ricardo alves da conceicao <pauloricardoalvesdaconceicao@gmail.com>
2026-09-24 21:30:22 -0300

    Adiciona aula 10 - rotas dinâmicas

---
+++ aula10-rotas-dinamicas/src/jogos.controller.ts
@@ -0,0 +1,14 @@
+
+import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
+import { JogosService } from "./jogos.service.js";
+
+
+@Controller('jogos')
+export class JogosController{
+    constructor(private readonly jogosService : JogosService){}
+    @Get(':id')
+    buscarPorId(@Param('id', ParseIntPipe) id:string){
+        const numId = +id;
+        return this.jogosService.buscarPorId(numId);
+    }
+}
